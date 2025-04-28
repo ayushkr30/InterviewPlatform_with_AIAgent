d1d592ff548ce6253d5027f004201872f29665e9
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-04-28 11:54:27 +0530

    routing, auth pages done.

---
+++ mock_interview_platform/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const MonaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "An AI-powered platform for preparing for mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${MonaSans.className}  antialiased pattern`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
